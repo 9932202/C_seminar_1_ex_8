bdad6cb7e77d5214c408182abc8de83e69cc058e
Анастасия Казарина <ana-kazarina@MacBook-Pro-Anastasia.local>
2023-01-03 11:14:05 +0300

    Сделано упраднение 8

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число больше 0");
+int number = int.Parse(Console.ReadLine());
+int avr = 2;
+while(avr < number){
+    Console.WriteLine(avr);
+    avr = avr + 2;
+}
